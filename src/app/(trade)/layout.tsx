bbbd842e64605f98c96a768635ad9e4ec5865dcc
Pranav <pranavkp.me@outlook.com>
2024-05-13 16:43:47 +0530

    fix the swap page not loading without login

--- src/app/(trade)/layout.tsx
+++ src/app/(trade)/layout.tsx
@@ -1,10 +1,7 @@
-import { checkAuth } from "@/lib/auth/utils";
-
 export default async function AppLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  await checkAuth();
   return <main>{children}</main>;
 }
--- src/app/(trade)/swap/page.tsx
+++ src/app/(trade)/swap/page.tsx
@@ -2,7 +2,7 @@ import { SwapDetails } from "@/components/swap/swapDetails";
 import { api } from "@/lib/trpc/api";
 
 const Page = async () => {
-  const { tokens } = await api.tokens.getTokens.query();
+  const { tokens } = await api.tokens.getAllTokens.query();
   return <SwapDetails tokens={tokens} />;
 };
 export default Page;
