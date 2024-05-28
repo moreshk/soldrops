019c421c6e0c35f3f6d02554986229f44a5aba2a
Pranav <pranavkp.me@outlook.com>
2024-05-28 10:16:26 +0530

    added strategy for connection

--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -1,4 +1,4 @@
 import { env } from "@/lib/env.mjs";
 import { Connection } from "@solana/web3.js";
 
-export const connection = new Connection(env.HELIUS_RPC_URL);
+export const connection = new Connection(env.HELIUS_RPC_URL, "confirmed");
